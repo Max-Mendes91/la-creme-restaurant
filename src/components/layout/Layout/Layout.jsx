b989ebfc76028da9beb3837a14c3f38e0f92fb6e
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-11-07 15:40:50 +0100

    feat: add dual-direction scroll-interactive golden accent background

--- src/components/layout/Layout/Layout.jsx
+++ src/components/layout/Layout/Layout.jsx
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 import Header from '../Header';
 import Footer from '../Footer';
+import ScrollingGoldenAccent from '../../effects/ScrollingGoldenAccent';
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <main>
+      <main style={{ position: 'relative' }}>
+        {/* Scroll-interactive golden accent background - appears from MenuSection onwards */}
+        <ScrollingGoldenAccent />
         {children}
       </main>
       <Footer />
